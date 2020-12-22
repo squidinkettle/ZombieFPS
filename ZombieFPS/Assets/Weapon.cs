2c46ca0448a1260e94efc618075d99a3c6eb5f27
squidinkettle <jchabol309@hotmail.com>
2020-12-22 15:45:11 +0100

    Animation

--- ZombieFPS/Assets/Weapon.cs
+++ ZombieFPS/Assets/Weapon.cs
@@ -9,27 +9,64 @@ public class Weapon : MonoBehaviour
     [SerializeReference] Camera FPCamera;
     [SerializeField] float range=100f;
     [SerializeField] int weaponDamage;
+    [SerializeField] ParticleSystem muzzle;
+    [SerializeField] GameObject explosion;
+    [SerializeField] float rateOfFire;
+    Animator animator;
+    bool isFiring;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(Shoot(rateOfFire));
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") || Input.GetButton("Fire1"))
         {
-            Shoot();
+            isFiring = true;
+            animator.SetBool("isShooting", true);
+        }
+        else
+        {
+            animator.SetBool("isShooting", false);
         }
     }
 
 
-    private void Shoot()
+    IEnumerator Shoot(float RoF)
     {
-        RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position,FPCamera.transform.forward,out hit, range);
-        if (hit.collider!=null || hit.collider.GetComponent<EnemyAI>())
+        while (true)
         {
-            DamageEnemy(hit);
+            while (isFiring)
+            {
+                RaycastHit hit;
+
+                Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+
+                muzzle.Play();
+                if (hit.collider != null && hit.collider.GetComponent<EnemyAI>())
+                {
+                    DamageEnemy(hit);
+                }
+                else
+                {
+                    HitEffect(hit);
+                }
+                yield return new WaitForSeconds(RoF);
+            }
+            yield return null;
         }
-       
+
+    }
+
+    private void HitEffect(RaycastHit hit)
+    {
+        GameObject hitFX= Instantiate(explosion, hit.point,Quaternion.LookRotation(hit.point));
+        Destroy(hitFX, 0.2f);
     }
 
     private void DamageEnemy(RaycastHit hit)
