2664ea3f925bfbfd177de7b249d7948e3d22ec4f
squidinkettle <jchabol309@hotmail.com>
2020-12-23 15:03:22 +0100

    Improved gun animation

--- ZombieFPS/Assets/Weapon.cs
+++ ZombieFPS/Assets/Weapon.cs
@@ -12,8 +12,11 @@ public class Weapon : MonoBehaviour
     [SerializeField] ParticleSystem muzzle;
     [SerializeField] GameObject explosion;
     [SerializeField] float rateOfFire;
+    [SerializeField] GameObject shell;
     Animator animator;
     bool isFiring;
+
+    Queue<GameObject> shellQueue=new Queue<GameObject>();
 
     private void Start()
     {
@@ -25,13 +28,15 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") || Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
             isFiring = true;
             animator.SetBool("isShooting", true);
+
         }
         else
         {
+            isFiring = false;
             animator.SetBool("isShooting", false);
         }
     }
@@ -43,10 +48,11 @@ public class Weapon : MonoBehaviour
         {
             while (isFiring)
             {
+
                 RaycastHit hit;
 
                 Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-
+                ShellQueueCicle();
                 muzzle.Play();
                 if (hit.collider != null && hit.collider.GetComponent<EnemyAI>())
                 {
@@ -57,9 +63,62 @@ public class Weapon : MonoBehaviour
                     HitEffect(hit);
                 }
                 yield return new WaitForSeconds(RoF);
+                animator.SetBool("isShooting", false);
             }
             yield return null;
         }
+
+    }
+
+    private void ShootOutShell()
+    {
+        float yOffset = 0.01f;
+        float xOffset = 0.04f;
+        Vector3 shellPosition = new Vector3(transform.position.x+xOffset, transform.position.y + yOffset, transform.position.z);
+        GameObject newShell = Instantiate(shell, shellPosition, Quaternion.identity);
+
+        shellQueue.Enqueue(newShell);
+        SetShellStartingPosition(newShell);
+
+
+
+    }
+
+    void ShellQueueCicle()
+    {
+        int maxNumShells = 10;
+        if (shellQueue.Count >= maxNumShells)
+        {
+            var oldShell = shellQueue.Dequeue();
+            SetShellStartingPosition(oldShell);
+
+
+            shellQueue.Enqueue(oldShell);
+
+
+                
+        }
+        else
+        {
+            ShootOutShell();
+        }
+
+
+    }
+
+    private void SetShellStartingPosition(GameObject showShell)
+    {
+        float popSpeed = 10f;
+        float yOffset = 0.01f;
+        float xOffset = 0.04f;
+        Vector3 shellPosition = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z);
+
+        showShell.transform.position=shellPosition;
+        showShell.transform.rotation = Quaternion.identity;
+        Rigidbody shellPhysics = showShell.GetComponent<Rigidbody>();
+
+        shellPhysics.AddForce(transform.up * popSpeed);
+        shellPhysics.AddForce(transform.right * popSpeed);
 
     }
 
